e8d6a30dee15e7abc0860fe3d83e93e2063d6e1b
melxin <melxin@tutamail.com>
2024-01-14 19:52:06 +0100

    JagexLauncherCredentials: update

--- src/main/java/com/openosrs/injector/injectors/raw/JagexLauncherCredentials.java
+++ src/main/java/com/openosrs/injector/injectors/raw/JagexLauncherCredentials.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2023, Melxin <https://github.com/melxin>
+ * Copyright (c) 2023 - 2024, Melxin <https://github.com/melxin>
  * All rights reserved.
  *
  * This code is licensed under GPL3, see the complete license in
@@ -8,8 +8,12 @@
 package com.openosrs.injector.injectors.raw;
 
 import java.util.List;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicInteger;
 import net.runelite.asm.ClassFile;
+import net.runelite.asm.ClassGroup;
 import net.runelite.asm.Method;
+import net.runelite.asm.Type;
 import net.runelite.asm.attributes.Code;
 import net.runelite.asm.attributes.code.Instruction;
 import net.runelite.asm.attributes.code.InstructionType;
@@ -22,6 +26,7 @@ import net.runelite.asm.attributes.code.instructions.LDC;
 import net.runelite.asm.attributes.code.instructions.PutStatic;
 import net.runelite.asm.attributes.code.instructions.Return;
 import net.runelite.asm.pool.Class;
+import net.runelite.asm.pool.Field;
 import net.runelite.asm.signature.Signature;
 import com.openosrs.injector.injection.InjectData;
 import com.openosrs.injector.injectors.AbstractInjector;
@@ -36,50 +41,103 @@ public class JagexLauncherCredentials extends AbstractInjector
 	@Override
 	public void inject()
 	{
+		final List<String> JX_PROPERTY_KEYS = List.of("JX_ACCESS_TOKEN", "JX_REFRESH_TOKEN", "JX_SESSION_ID", "JX_CHARACTER_ID", "JX_DISPLAY_NAME");
+
+		final ClassGroup vanillaGroup = inject.getVanilla();
+
 		final ClassFile clientVanilla = inject.toVanilla(
 			inject.getDeobfuscated()
 				.findClass("Client")
 		);
 
-		final Method init = clientVanilla.findMethod("init");
 		final Method getStaticCredentialsProperty = new Method(clientVanilla, "getStaticCredentialsProperty", new Signature("(Ljava/lang/String;)Ljava/lang/String;"));
 
-		final Instructions instructions = init.getCode().getInstructions();
-		final List<Instruction> ins = instructions.getInstructions();
+		final Type clientType = new Type("Lclient;");
 
-		boolean insert = false;
-		for (int i = 0; i < ins.size(); i++)
+		final AtomicBoolean createdGetStaticCredentialsProperty = new AtomicBoolean(false);
+		final AtomicInteger replacedEnvCount = new AtomicInteger(0);
+
+		Field staticClient = clientVanilla.findMethod("init").getCode().getInstructions().getInstructions().stream()
+			.filter(in -> in instanceof PutStatic && ((PutStatic) in).getMyField().getObfuscatedType().equals(clientType))
+			.map(in -> ((PutStatic) in).getField())
+			.findFirst()
+			.orElse(null);
+
+		for (ClassFile cf : vanillaGroup)
 		{
-			Instruction in = ins.get(i);
-
-			if (in instanceof PutStatic && ((PutStatic) in).getMyField().getObfuscatedType().toString().startsWith("Lclient;"))
+			if (!createdGetStaticCredentialsProperty.get())
 			{
-				getStaticCredentialsProperty.setPublic();
-				getStaticCredentialsProperty.setStatic(true);
-				Code code = new Code(getStaticCredentialsProperty);
-				code.setMaxStack(2);
-				getStaticCredentialsProperty.setCode(code);
-				List<Instruction> getStaticCredentialsPropertyIns = code.getInstructions().getInstructions();
-				getStaticCredentialsPropertyIns.add(new GetStatic(instructions, ((PutStatic) in).getField()));
-				getStaticCredentialsPropertyIns.add(new ALoad(instructions, 0));
-				getStaticCredentialsPropertyIns.add(new InvokeInterface(instructions, new net.runelite.asm.pool.Method(new Class("net/runelite/rs/api/RSClient"), "getCredentialsProperty", new Signature("(Ljava/lang/String;)Ljava/lang/String;"))));
-				getStaticCredentialsPropertyIns.add(new Return(instructions, InstructionType.ARETURN));
-				clientVanilla.addMethod(getStaticCredentialsProperty);
+				if (staticClient == null)
+				{
+					staticClient = cf.getFields().stream()
+						.filter(f -> f.isStatic() && f.getObfuscatedType().equals(clientType))
+						.map(f -> f.getPoolField())
+						.findFirst()
+						.orElse(null);
+				}
+
+				if (staticClient != null)
+				{
+					//System.out.println("Found static client: " + staticClient);
+					getStaticCredentialsProperty.setPublic();
+					getStaticCredentialsProperty.setStatic(true);
+					Code code = new Code(getStaticCredentialsProperty);
+					code.setMaxStack(2);
+					getStaticCredentialsProperty.setCode(code);
+					Instructions instructions = code.getInstructions();
+					List<Instruction> ins = instructions.getInstructions();
+					ins.add(new GetStatic(instructions, staticClient));
+					ins.add(new ALoad(instructions, 0));
+					ins.add(new InvokeInterface(instructions, new net.runelite.asm.pool.Method(new Class("net/runelite/rs/api/RSClient"), "getCredentialsProperty", new Signature("(Ljava/lang/String;)Ljava/lang/String;"))));
+					ins.add(new Return(instructions, InstructionType.ARETURN));
+					clientVanilla.addMethod(getStaticCredentialsProperty);
+
+					createdGetStaticCredentialsProperty.set(true);
+				}
 			}
 
-			if (in instanceof LDC && ((LDC) in).getConstant() instanceof String && ((String) ((LDC) in).getConstant()).startsWith("JX_"))
+			if (replacedEnvCount.get() != JX_PROPERTY_KEYS.size())
 			{
-				//System.out.println("LDC: " + ((LDC) in).getConstant());
-				insert = true;
+				cf.getMethods().stream()
+					.filter(m -> m.getCode() != null && m.getCode().getInstructions().getInstructions().stream()
+						.filter(in -> in instanceof LDC && ((LDC) in).getConstant() instanceof String && JX_PROPERTY_KEYS.contains(((LDC) in).getConstant()))
+						.count() == JX_PROPERTY_KEYS.size())
+					.findFirst()
+					.ifPresent(targetMethod ->
+					{
+						final Code code = targetMethod.getCode();
+						final Instructions instructions = code.getInstructions();
+						final List<Instruction> ins = instructions.getInstructions();
+
+						for (int i = 0; i < ins.size(); i++)
+						{
+							Instruction in = ins.get(i);
+
+							if (in instanceof LDC && ((LDC) in).getConstant() instanceof String && JX_PROPERTY_KEYS.contains(((LDC) in).getConstant()))
+							{
+								//System.out.println("LDC: " + ((LDC) in).getConstant());
+								Instruction shouldBeInvokeStatic = i + 1 < ins.size() ? ins.get(i + 1) : null;
+								if (shouldBeInvokeStatic != null && shouldBeInvokeStatic instanceof InvokeStatic && ((InvokeStatic) shouldBeInvokeStatic).getMethod().getName().equals("getenv"))
+								{
+									ins.set(i + 1, new InvokeStatic(instructions, getStaticCredentialsProperty));
+									replacedEnvCount.getAndIncrement();
+									//System.out.println("Replace: " + " cf: " + cf + " method: " + ((InvokeStatic) in).getMethod() + " -> " + getStaticCredentialsProperty);
+								}
+							}
+						}
+					});
 			}
 
-			if (insert && in instanceof InvokeStatic && ((InvokeStatic) in).getMethod().getName().equals("getenv"))
+			if (createdGetStaticCredentialsProperty.get() && replacedEnvCount.get() == JX_PROPERTY_KEYS.size())
 			{
-				ins.set(i, new InvokeStatic(instructions, getStaticCredentialsProperty));
-				insert = false;
-				//System.out.println("Replace method: " + ((InvokeStatic) in).getMethod().getName() + " -> " + getStaticCredentialsProperty.getName());
+				System.out.println("JagexLauncherCredentials injection was successfully," + " replaced " + JX_PROPERTY_KEYS + " with " + getStaticCredentialsProperty);
+				break;
 			}
 		}
 
+		if (!createdGetStaticCredentialsProperty.get() || replacedEnvCount.get() != JX_PROPERTY_KEYS.size())
+		{
+			System.err.println("JagexLauncherCredentials injection was unsuccessfully, manual fix required!");
+		}
 	}
 }
